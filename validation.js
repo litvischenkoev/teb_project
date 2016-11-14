18037edcbf46038d3f10a5ebe710647fff0b8068
litvischenkoev <litvischenkoev@gmail.com>
2016-11-14 21:04:04 +0200

    controller data

--- validation.js
+++ validation.js
@@ -20,6 +20,42 @@ weather.controller('validationCtrl', function validationCtrl($scope) {
 		console.log(data);
 	}
 	
+//for calendar
+	$scope.today = function() {
+    	$scope.dt = new Date();
+  	};
+  	
+  	$scope.today();
+
+  	$scope.clear = function() {
+    	$scope.dt = null;
+  	};
+
+  	$scope.open1 = function() {
+    	$scope.popup1.opened = true;
+  	};
+
+  	$scope.popup1 = {
+    	opened: false
+  	};
+
+  	function getDayClass(data) {
+    	var date = data.date,
+      	mode = data.mode;
+    	if (mode === 'day') {
+      		var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      		for (var i = 0; i < $scope.events.length; i++) {
+        		var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        		if (dayToCheck === currentDay) {
+          			return $scope.events[i].status;
+        		}
+      		}
+    	}
+
+    	return '';
+  	}
 });
 
 
@@ -55,40 +91,5 @@ weather.directive('lowerCase', function () {
 	 }
 });
 
-weather.controller('DatepickerPopupDemoCtrl', function DatepickerPopupDemoCtrl($scope) {
-  console.log('DatepickerPopupDemoCtrl');
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
 
-  $scope.clear = function() {
-    $scope.dt = null;
-  };
 
-  $scope.open1 = function() {
-    $scope.popup1.opened = true;
-  };
-
-  $scope.popup1 = {
-    opened: false
-  };
-
-  function getDayClass(data) {
-    var date = data.date,
-      mode = data.mode;
-    if (mode === 'day') {
-      var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-      for (var i = 0; i < $scope.events.length; i++) {
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
-    }
-
-    return '';
-  }
-});
